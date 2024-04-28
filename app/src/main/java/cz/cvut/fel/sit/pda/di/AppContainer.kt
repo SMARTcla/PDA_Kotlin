bcae322f466981ec2d68bf0eaf7851d149a361db
Kross Aleksandr <krossale@fel.cvut.cz>
2024-04-28 18:06:15 +0200

    create AppContainer/ need to finish

---
+++ app/src/main/java/cz/cvut/fel/sit/pda/di/AppContainer.kt
@@ -0,0 +1,19 @@
+package cz.cvut.fel.sit.pda.di
+
+//TODO
+import android.content.Context
+
+//interface AppContainer {
+//    val userProfileRepository: UserProfileRepository
+//
+//}
+//
+//class AppDataContainer(private val context: Context) : AppContainer {
+//
+//    override val userProfileRepository: UserProfileRepository by lazy {
+//        UserProfileRepository(context)
+//    }
+//
+//
+//
+//}
