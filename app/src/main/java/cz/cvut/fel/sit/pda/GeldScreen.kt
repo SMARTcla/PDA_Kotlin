bd5bb98fd3f8adc0d221d3210476f32f78e0c5cf
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-02 01:24:11 +0200

    add eNum GeldScreen

---
+++ app/src/main/java/cz/cvut/fel/sit/pda/GeldScreen.kt
@@ -0,0 +1,23 @@
+package cz.cvut.fel.sit.pda
+
+enum class GeldScreen() {
+    Accounts,
+    Overview,
+    Transactions,
+    Budget,
+    Categories,
+    AddTransaction,
+    Settings,
+    AddAccounts,
+    Income,
+    Expenses,
+    Notifications,
+    Version,
+    Rating,
+    Support
+
+
+
+    // Add new values in future
+
+}
