239fae5263e63991a1cdb02486f96241aa762b00
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-05 17:44:59 +0200

    add color style for AccountsScreen

--- app/src/main/java/cz/cvut/fel/sit/pda/MainActivity.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/MainActivity.kt
@@ -2,33 +2,10 @@ package cz.cvut.fel.sit.pda
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import cz.cvut.fel.sit.pda.models.Transaction
-import cz.cvut.fel.sit.pda.screens.AddTransactionScreen
-import cz.cvut.fel.sit.pda.screens.AccountsScreen
 import cz.cvut.fel.sit.pda.ui.theme.PDATheme
-import cz.cvut.fel.sit.pda.screens.TransactionsScreen
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
