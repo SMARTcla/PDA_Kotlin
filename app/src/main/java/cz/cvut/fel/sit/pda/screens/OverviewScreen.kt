aa7889d0fe38b0aeb0330eea73b8542d9577e377
Mikhailo Kononenko <kononmi1@fel.cvut.cz>
2024-05-05 14:11:35 +0200

    Change Transaction Details and Edit Transaction

--- app/src/main/java/cz/cvut/fel/sit/pda/screens/OverviewScreen.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/screens/OverviewScreen.kt
@@ -14,8 +14,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,7 +29,7 @@ import cz.cvut.fel.sit.pda.components.GeldsBottomBar
 import cz.cvut.fel.sit.pda.models.Transaction
 import cz.cvut.fel.sit.pda.models.TransactionType
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun OverviewScreen(navController: NavHostController, transactions: MutableList<Transaction>) {
     var isExpensesSelected by remember { mutableStateOf(true) }
