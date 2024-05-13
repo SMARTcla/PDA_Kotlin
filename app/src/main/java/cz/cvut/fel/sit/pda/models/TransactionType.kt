d54eeb95c5452dc6054d426d269ea138502f7bf1
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-13 19:15:35 +0200

    update TransactionType

--- app/src/main/java/cz/cvut/fel/sit/pda/models/TransactionType.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/models/TransactionType.kt
@@ -4,20 +4,16 @@ enum class TransactionType(val displayName: String, val category: String) {
     // Expences
     RESTAURANT("Restaurant", "Expenses"),
     GROCERIES("Groceries", "Expenses"),
-    SUBSCRIPTIONS("Subscriptions", "Expenses"),
-    ENTERTAINMENT("Entertainment", "Expenses"),
-    UTILITIES("Utilities", "Expenses"),
-    RENT("Rent", "Expenses"),
+    LEISURE("Leisure", "Expenses"),
+    FAMILY("Family", "Expenses"),
     TRANSPORT("Transport", "Expenses"),
     HEALTHCARE("Healthcare", "Expenses"),
-    INSURANCE("Insurance", "Expenses"),
     GIFTS("Gifts", "Expenses"),
-    CLOTHING("Clothing", "Expenses"),
-    EDUCATION("Education", "Expenses"),
-    PERSONAL_CARE("Personal Care", "Expenses"),
-    TRAVEL("Travel", "Expenses"),
-    OTHER("Other", "Expenses"),
+    SHOPPING("Shopping", "Expenses"),
+    OTHER_EXP("Other", "Expenses"),
     // Income
     SALARY("Salary", "Income"),
-    BENEFITS("Benefits", "Income");
+    BENEFITS("Benefits", "Income"),
+    OTHER_INC("Other", "Income"),
+
 }
