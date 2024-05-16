28e06968877b72cb0c3dd01c2fe66475cce87ed9
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-16 09:08:58 +0200

    hotfix models

--- app/src/main/java/cz/cvut/fel/sit/pda/models/Transaction.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/models/Transaction.kt
@@ -1,14 +1,30 @@
 package cz.cvut.fel.sit.pda.models
 
+import cz.cvut.fel.sit.pda.database.TransactionEntity
+import cz.cvut.fel.sit.pda.database.TransactionType
 import java.time.LocalDate
-import java.util.UUID
 
 data class Transaction(
-    val id: String = UUID.randomUUID().toString(),
+    val id: String,
     val name: String,
-    val amount: Double,
+    val amount: Long,
     val type: TransactionType,
     val date: LocalDate,
-    val category: String,
     val cardName: String
-)
+) {
+
+    private fun convertDateLocalToDateLong(): Long {
+        return (date.dayOfMonth * 1000000 + (date.monthValue + 1) * 10000 + date.year).toLong()
+    }
+
+    fun toEntity(): TransactionEntity {
+        return TransactionEntity(
+            id = id.toLong(),
+            name = name,
+            amount = amount,
+            type = type,
+            date = convertDateLocalToDateLong(),
+            card = cardName,
+        )
+    }
+}
