f208f52b874f62300999defeb2cd6ff81abf63db
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-16 09:10:38 +0200

    hotfix MainActivity

--- app/src/main/java/cz/cvut/fel/sit/pda/MainActivity.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/MainActivity.kt
@@ -3,38 +3,68 @@ package cz.cvut.fel.sit.pda
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import cz.cvut.fel.sit.pda.database.GeldDatabase
+import cz.cvut.fel.sit.pda.database.GeldViewModel
+import cz.cvut.fel.sit.pda.database.ViewStates
 import cz.cvut.fel.sit.pda.services.NotificationService.Companion.CHANNEL_ID
 import cz.cvut.fel.sit.pda.ui.theme.PDATheme
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
+    private lateinit var navHostController: NavHostController
+
+
+    val db by lazy { GeldDatabase.getInstance(this) }
+
+    private val viewModel: GeldViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return GeldViewModel(db.transactionDao, db.bankDao) as T
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PDATheme {
-                AppNavigation()
-            }
-        }
+                viewModel.fetchDataOnStart()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Reminder Channel"
-            val descriptionText = "Channel for notifications"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+                navHostController = rememberNavController()
 
+                AppNavigation(
+                    navController = navHostController,
+                    geldViewModel = viewModel
+//                    onEvent = viewModel::onEvent
+                )
+            }
+
+            createNotificationChannel()
+//            viewModel.initializeData()
+        }
+    }
+
+    private fun createNotificationChannel() {
+        val name = "Reminder Channel"
+        val descriptionText = "Channel for notifications"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
