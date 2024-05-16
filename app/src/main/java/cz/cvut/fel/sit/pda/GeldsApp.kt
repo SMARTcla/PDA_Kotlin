1909a611fd8742dfe0e1572b6ea12db626d3cfe1
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-16 09:10:38 +0200

    hotfix MainActivity

--- app/src/main/java/cz/cvut/fel/sit/pda/GeldsApp.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/GeldsApp.kt
@@ -2,121 +2,191 @@ package cz.cvut.fel.sit.pda
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import cz.cvut.fel.sit.pda.database.GeldViewModel
 import cz.cvut.fel.sit.pda.models.BankCard
 import cz.cvut.fel.sit.pda.models.Transaction
 import cz.cvut.fel.sit.pda.screens.accounts.AccountsScreen
 import cz.cvut.fel.sit.pda.screens.accounts.AddCardScreen
-import cz.cvut.fel.sit.pda.screens.transactions.AddTransactionScreen
 import cz.cvut.fel.sit.pda.screens.accounts.CardDetailScreen
 import cz.cvut.fel.sit.pda.screens.accounts.EditCardScreen
+import cz.cvut.fel.sit.pda.screens.accounts.deleteCard
 import cz.cvut.fel.sit.pda.screens.category.CategoriesScreen
-import cz.cvut.fel.sit.pda.screens.transactions.EditTransactionScreen
 import cz.cvut.fel.sit.pda.screens.overview.OverviewScreen
 import cz.cvut.fel.sit.pda.screens.settings.SettingsScreen
+import cz.cvut.fel.sit.pda.screens.transactions.AddTransactionScreen
+import cz.cvut.fel.sit.pda.screens.transactions.EditTransactionScreen
 import cz.cvut.fel.sit.pda.screens.transactions.TransactionDetailScreen
+import cz.cvut.fel.sit.pda.screens.transactions.TransactionScreenUiState
 import cz.cvut.fel.sit.pda.screens.transactions.TransactionsScreen
-import cz.cvut.fel.sit.pda.screens.accounts.deleteCard
-import cz.cvut.fel.sit.pda.screens.transactions.deleteTransaction
-import cz.cvut.fel.sit.pda.ui.theme.PDATheme
+import cz.cvut.fel.sit.pda.screens.transactions.ui.TransactionViewModel
+import cz.cvut.fel.sit.pda.screens.transactions.ui.TransactionViewModelFactory
+import kotlinx.coroutines.launch
 
 @SuppressLint("ComposableDestinationInComposeScope")
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-    val transactions = remember { mutableStateListOf<Transaction>() }
-    val cards = remember { mutableStateListOf<BankCard>() }
+fun AppNavigation(
+    navController: NavHostController,
+    geldViewModel: GeldViewModel,
+//    onEvent: (GeldEvent) -> Boolean
+) {
+    val appUiState by geldViewModel.uiState.collectAsStateWithLifecycle()
+//    val notificationEnabled = remember { mutableStateOf(getNotificationEnabled(context)) }
 
     NavHost(navController = navController,
         startDestination = GeldScreen.Accounts.name,
-        modifier = Modifier) {
+        modifier = Modifier
+    ) {
 
         composable(GeldScreen.Accounts.name) {
-            AccountsScreen(navController, transactions, cards) }
+            AccountsScreen(
+                navController,
+                appUiState.transactions,
+                appUiState.cards
+            )
+        }
 
         composable(GeldScreen.AddCardScreen.name) {
-            AddCardScreen(navController, cards) }
+            val coroutineScope = rememberCoroutineScope()
+
+            AddCardScreen(
+                navController = navController,
+                cards = appUiState.cards,
+                saveBank = { bank ->
+                    coroutineScope.launch {
+                        geldViewModel.saveBank(bank)
+                    }
+                }
+            )
+        }
 
         composable(GeldScreen.Overview.name) {
-            OverviewScreen(navController, transactions) }
+            OverviewScreen(navController, appUiState.transactions) }
 
         composable(GeldScreen.Transactions.name) {
-            TransactionsScreen(navController, transactions) }
+            TransactionsScreen(navController, appUiState.transactions)
+        }
+
         composable(GeldScreen.Categories.name) {
-            CategoriesScreen(navController, transactions) }
+            CategoriesScreen(navController, appUiState.transactions) }
 
         composable(GeldScreen.Settings.name) {
             SettingsScreen(navController) }
 
+//        composable(GeldScreen.Notifications.name) {
+//            NotificationsScreen(navController, notificationEnabled.value) { enabled ->
+//                notificationEnabled.value = enabled
+//                val editor = context.getSharedPreferences("AppSettings",
+//                    Context.MODE_PRIVATE).edit()
+//                editor.putBoolean("NotificationsEnabled", enabled)
+//                editor.apply()
+//            }
+//        }
+
         composable(GeldScreen.AddTransaction.name) {
-            AddTransactionScreen(navController, { transaction ->
-                transactions.add(transaction)
-                navController.popBackStack()
-            }, cards)
+            val viewModel = viewModel<TransactionViewModel>()
+            val coroutineScope = rememberCoroutineScope()
+
+            AddTransactionScreen(
+                navController = navController,
+                viewModel = viewModel,
+                saveTransaction = { transaction ->
+                    coroutineScope.launch {
+                        geldViewModel.saveTransaction(transaction)
+                    }
+                    navController.popBackStack()
+                },
+                cards = appUiState.banks.map { BankCard(it.name) }
+            )
         }
 
-        composable(GeldScreen.TransactionDetails.name + "/{transactionId}") { backStackEntry ->
+        composable("transactionDetail/{transactionId}") { backStackEntry ->
             val transactionId = backStackEntry.arguments?.getString("transactionId") ?: return@composable
-            val transaction = transactions.find { it.id == transactionId }
+            val transaction = appUiState.transactions.find { it.id == transactionId }
+
+            val coroutineScope = rememberCoroutineScope()
+
             if (transaction != null) {
-                TransactionDetailScreen(navController, transaction, transactions) { transactionToDelete ->
-                    deleteTransaction(transactionToDelete, transactions)
+                TransactionDetailScreen(navController, transaction) {
+                    coroutineScope.launch {
+                        geldViewModel.deleteTransaction(transaction)
+                    }
                 }
             } else {
-                // Handle error case
+
             }
         }
-
-        composable(GeldScreen.CardDetails.name + "/{cardName}") { backStackEntry ->
+        composable("cardDetails/{cardName}") { backStackEntry ->
             backStackEntry.arguments?.getString("cardName")?.let { cardName ->
-                val card = cards.find { it.name == cardName }
+                val card = appUiState.cards.find { it.name == cardName }
                 if (card != null) {
                     CardDetailScreen(navController, card) { cardToDelete ->
-                        deleteCard(cardToDelete, cards)
+                        deleteCard(cardToDelete, appUiState.cards.toMutableList())
                     }
                 } else {
-                    // Handle error case
+
                 }
             }
         }
 
 
-        composable(GeldScreen.EditTransaction.name + "/{transactionId}") { backStackEntry ->
+        composable("editTransaction/{transactionId}") { backStackEntry ->
             val transactionId = backStackEntry.arguments?.getString("transactionId") ?: return@composable
-            val transaction = transactions.find { it.id == transactionId }
+            val transaction = appUiState.transactions.find { it.id == transactionId }
+            val coroutineScope = rememberCoroutineScope()
+
+            val viewModel = viewModel<TransactionViewModel>(
+                factory = transaction?.let {
+                    TransactionViewModelFactory(
+                        TransactionScreenUiState(
+                            name = transaction.name,
+                            amount = transaction.amount.toString(),
+                            type = transaction.type,
+                            date = transaction.date,
+                            cardName = transaction.cardName,
+                            transactionId = transaction.id.toLong()
+                        )
+                    )
+                }
+            )
+
             if (transaction != null) {
-                EditTransactionScreen(navController, transaction, transactions, cards)
+                EditTransactionScreen(
+                    navController = navController,
+                    viewModel = viewModel,
+                    banks = appUiState.cards,
+                    updateTransaction = {
+                        coroutineScope.launch {
+                            geldViewModel.updateTransaction(it)
+                        }
+                    }
+                )
             } else {
-                // Handle error case
+
             }
         }
 
-        composable(GeldScreen.EditCardScreen.name + "/{cardName}") { backStackEntry ->
-            val cardName = backStackEntry.arguments?.getString("cardName") ?: return@composable
-            val card = cards.find { it.name == cardName }
-            if (card != null) {
-                EditCardScreen(navController, card, onUpdate = { updatedCard ->
-                    val index = cards.indexOfFirst { it.name == card.name }
-                    if (index != -1) {
-                        cards[index] = updatedCard
-                    }
-                })
-            } else {
-                // Handle error case
-            }
-        }
+//        composable("editCard/{cardName}") { backStackEntry ->
+//            val cardName = backStackEntry.arguments?.getString("cardName") ?: return@composable
+//            val card = appUiState.cards.find { it.name == cardName }
+//            if (card != null) {
+//                EditCardScreen(navController, card, onUpdate = { updatedCard ->
+//                    val index = appUiState.cards.indexOfFirst { it.name == card.name }
+//                    /*if (index != -1) {
+//                        appUiState.cards.get(index) = updatedCard
+//                    }*/
+//                })
+//            } else {
+//            }
+//        }
     }
 }
 
@@ -128,11 +198,3 @@ fun MutableList<Transaction>.updateTransaction(updatedTransaction: Transaction) 
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
-@Preview(showBackground = true)
-@Composable
-fun PreviewGreetingScreen() {
-    PDATheme {
-        AppNavigation()
-    }
-}
