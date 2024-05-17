96ff2ae834a5d709dbaac223a820a451bed7e0a0
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-17 03:05:18 +0200

    update Settings Screen

--- app/src/main/java/cz/cvut/fel/sit/pda/screens/settings/SettingsScreen.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/screens/settings/SettingsScreen.kt
@@ -1,5 +1,7 @@
 package cz.cvut.fel.sit.pda.screens.settings
 
+import MyReceiptsDialog
+import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -27,6 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import cz.cvut.fel.sit.pda.screens.settings.receipts.AddReceiptsDialog
 import cz.cvut.fel.sit.pda.ui.theme.PDATheme
 import kotlinx.coroutines.launch
 
@@ -37,7 +40,10 @@ fun SettingsScreen(navController: NavHostController) {
     var showAboutDialog by remember { mutableStateOf(false) }
     var showSupportDialog by remember { mutableStateOf(false) }
     var showNotificationsDialog by remember { mutableStateOf(false) }
+    var showAddReceiptsDialog by remember { mutableStateOf(false) }
+    var showMyReceiptsDialog by remember { mutableStateOf(false) }
     var enableNotifications by remember { mutableStateOf(false) }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
@@ -67,6 +73,8 @@ fun SettingsScreen(navController: NavHostController) {
             SettingsContent(
                 navController, paddingValues,
                 onNotificationsClick = { showNotificationsDialog = true },
+                onAddReceiptsClick = { showAddReceiptsDialog = true },
+                onMyReceiptsClick = { showMyReceiptsDialog = true },
                 onVersionClick = { showVersionDialog = true },
                 onAboutClick = { showAboutDialog = true },
                 onSupportClick = { showSupportDialog = true },
@@ -90,6 +98,16 @@ fun SettingsScreen(navController: NavHostController) {
                     onDismiss = { showNotificationsDialog = false }
                 )
             }
+
+            if (showAddReceiptsDialog) {
+                AddReceiptsDialog(
+                    onDismiss = { showAddReceiptsDialog = false },
+                    onPictureSelected = { uri -> imageUri = uri }
+                )
+            }
+            if (showMyReceiptsDialog && imageUri != null) {
+                MyReceiptsDialog(imageUri = imageUri!!, onDismiss = { showMyReceiptsDialog = false })
+            }
         }
     }
 }
@@ -98,6 +116,8 @@ fun SettingsScreen(navController: NavHostController) {
 fun SettingsDrawerContent(navController: NavHostController, onDestinationClicked: () -> Unit) {
     Column(modifier = Modifier.padding(start = 24.dp, top = 48.dp)) {
         Text("Notifications", modifier = Modifier.clickable(onClick = onDestinationClicked))
+        Text("Add receipts", modifier = Modifier.clickable(onClick = onDestinationClicked))
+        Text("My receipts", modifier = Modifier.clickable(onClick = onDestinationClicked))
         Text("Version", modifier = Modifier.clickable(onClick = onDestinationClicked))
         Text("About us", modifier = Modifier.clickable(onClick = onDestinationClicked))
         Text("Support", modifier = Modifier.clickable(onClick = onDestinationClicked))
@@ -109,12 +129,16 @@ fun SettingsContent(
     navController: NavHostController,
     paddingValues: PaddingValues,
     onNotificationsClick: () -> Unit,
+    onAddReceiptsClick: () -> Unit,
+    onMyReceiptsClick: () -> Unit,
     onVersionClick: () -> Unit,
     onAboutClick: () -> Unit,
     onSupportClick: () -> Unit
 ) {
     Column(modifier = Modifier.padding(paddingValues)) {
         OptionItem("Notifications", onClick = onNotificationsClick)
+        OptionItem("Add receipts", onClick = onAddReceiptsClick)
+        OptionItem("My receipts", onClick = onMyReceiptsClick)
         OptionItem("Version", onClick = onVersionClick)
         OptionItem("About us", onClick = onAboutClick)
         OptionItem("Support", onClick = onSupportClick)
