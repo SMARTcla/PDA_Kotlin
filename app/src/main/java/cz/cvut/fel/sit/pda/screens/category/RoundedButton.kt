d4946f7c4ed0f11e751d8610f4d91445bf2701a4
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-17 23:17:09 +0200

    update RoundedButton style

--- app/src/main/java/cz/cvut/fel/sit/pda/screens/category/RoundedButton.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/screens/category/RoundedButton.kt
@@ -1,6 +1,7 @@
 package cz.cvut.fel.sit.pda.screens.category
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
@@ -9,15 +10,43 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import cz.cvut.fel.sit.pda.ui.theme.DeepPurple500
-import cz.cvut.fel.sit.pda.ui.theme.Indigo50
+import cz.cvut.fel.sit.pda.ui.theme.*
+
+const val TOTAL_EXPENSES = "Total Expenses:\n"
+const val TOTAL_INCOME = "Total Income:\n"
+const val CURRENCY = " CZK\n\n"
+const val GO_TO_INCOME = "Go to Income"
+const val GO_TO_EXPENSES = "Go to Expenses"
 
 @Composable
 fun RoundedButton(isExpensesSelected: Boolean, totalAmount: Long, onToggle: () -> Unit) {
+    val expensesText = buildAnnotatedString {
+        append(TOTAL_EXPENSES)
+        pushStyle(SpanStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+        append("$totalAmount")
+        pop()
+        append(CURRENCY)
+        pushStyle(SpanStyle(color = Color.Yellow))
+        append(GO_TO_INCOME)
+    }
+
+    val incomeText = buildAnnotatedString {
+        append(TOTAL_INCOME)
+        pushStyle(SpanStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+        append("$totalAmount")
+        pop()
+        append(CURRENCY)
+        pushStyle(SpanStyle(color = Color.Yellow))
+        append(GO_TO_EXPENSES)
+    }
+
     Button(
         onClick = { onToggle() },
         modifier = Modifier
@@ -30,13 +59,14 @@ fun RoundedButton(isExpensesSelected: Boolean, totalAmount: Long, onToggle: () -
         )
     ) {
         Text(
-            text = if (isExpensesSelected) "Total: " +
-                    "$totalAmount CZK\n\nGo to Income" else "Total: $totalAmount CZK\n\nGo to Expenses",
+            text = if (isExpensesSelected) expensesText else incomeText,
             style = TextStyle(
                 color = Indigo50,
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold
-            )
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
