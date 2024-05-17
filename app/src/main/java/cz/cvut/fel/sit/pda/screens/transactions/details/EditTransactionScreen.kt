aefbc379c8440cd78492cc5ee6f52620dbe5a13e
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-17 20:58:06 +0200

    fix edit Income/Expenses categories

--- app/src/main/java/cz/cvut/fel/sit/pda/screens/transactions/details/EditTransactionScreen.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/screens/transactions/details/EditTransactionScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -54,7 +55,9 @@ fun EditTransactionScreen(
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    val isExpensesSelected by remember { mutableStateOf(true) }
+    var isExpensesSelected by remember {
+        mutableStateOf(uiState.type.category == "Expenses")
+    }
     val context = LocalContext.current
 
     Scaffold(
