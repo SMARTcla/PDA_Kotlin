a19b9ff0db5a85fd01fe30b00912b618fc1f723f
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-18 12:42:00 +0200

    update KDoc Services package

--- app/src/main/java/cz/cvut/fel/sit/pda/services/NotificationReceiver.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/services/NotificationReceiver.kt
@@ -7,14 +7,32 @@ import android.content.Intent
 import androidx.core.app.NotificationCompat
 import cz.cvut.fel.sit.pda.R
 
+/**
+ * NotificationReceiver is a BroadcastReceiver that handles the triggering of notifications.
+ *
+ * This receiver is activated by an alarm set by the NotificationService to display a reminder
+ * notification to the user.
+ */
 class NotificationReceiver : BroadcastReceiver() {
+
+    companion object {
+        const val CONTENT_TITLE = "Reminder"
+        const val CONTENT_TEXT = "Please back to App"
+    }
+
+    /**
+     * Called when the BroadcastReceiver is receiving an Intent broadcast.
+     *
+     * @param context The Context in which the receiver is running.
+     * @param intent The Intent being received.
+     */
     override fun onReceive(context: Context?, intent: Intent?) {
         val notificationManager =
             context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val builder = NotificationCompat.Builder(context, NotificationService.CHANNEL_ID)
             .setSmallIcon(R.drawable.notification)
-            .setContentTitle("Reminder")
-            .setContentText("Please back to App")
+            .setContentTitle(CONTENT_TITLE)
+            .setContentText(CONTENT_TEXT)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
 
--- app/src/main/java/cz/cvut/fel/sit/pda/services/NotificationService.kt
+++ app/src/main/java/cz/cvut/fel/sit/pda/services/NotificationService.kt
@@ -2,26 +2,48 @@ package cz.cvut.fel.sit.pda.services
 
 import android.annotation.SuppressLint
 import android.app.AlarmManager
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
-import androidx.core.app.NotificationCompat
-import cz.cvut.fel.sit.pda.R
 
+/**
+ * NotificationService is a background service that schedules a notification
+ * to be shown after a specified delay.
+ */
 class NotificationService : Service() {
+
+    companion object {
+        const val CHANNEL_ID = "channel_reminder"
+        const val NOTIFICATION_ID = 1
+        const val NOTIFICATION_DELAY_MS = 6000L // 6 seconds
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
+    /**
+     * Called when the service is started with startService().
+     *
+     * @param intent The Intent supplied to startService(), as given.
+     * @param flags Additional data about this start request.
+     * @param startId A unique integer representing this specific request to start.
+     * @return The return value indicates how the system should continue the service
+     *         in case it is killed: always returns START_NOT_STICKY.
+     */
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         scheduleNotification()
         return START_NOT_STICKY
     }
 
+    /**
+     * Schedules a notification to be triggered after a specified delay.
+     *
+     * The notification is scheduled using the AlarmManager to trigger after
+     * 10 seconds (6000 milliseconds).
+     */
     @SuppressLint("ServiceCast")
     private fun scheduleNotification() {
         val notificationIntent = Intent(this, NotificationReceiver::class.java)
@@ -32,14 +54,9 @@ class NotificationService : Service() {
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
-        val futureInMillis = System.currentTimeMillis() + 6000 // 10 second
+        val futureInMillis = System.currentTimeMillis() + NOTIFICATION_DELAY_MS
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.set(AlarmManager.RTC_WAKEUP, futureInMillis, pendingIntent)
-    }
-
-    companion object {
-        const val CHANNEL_ID = "channel_reminder"
-        const val NOTIFICATION_ID = 1
     }
 }
 
